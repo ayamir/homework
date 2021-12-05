48e3b47b556fc74b9e872d21ccfe683718e24ae2
ayamir <lgt986452565@gmail.com>
2021-12-05 11:39:38 +0800

    refactor(crawling): extract xpath variables to file.

---
+++ xpath.py
@@ -0,0 +1,13 @@
+login_button_xpath = '//*[@id="pl_login_form"]/div/div[3]/div[6]/a'
+dm_check_xpath = '//*[@id="dmCheck"]'
+dm_send_button_xpath = '//*[@id="send_dm_btn"]'
+
+info_link_xpath = '//*[@id="pl_service_showcomplaint"]/table/tbody/tr[%s]/td[2]/div/a'
+reporter_xpath = '//*[@id="pl_service_showcomplaint"]/table/tbody/tr[%s]/td[3]/a'
+reportee_xpath = '//*[@id="pl_service_showcomplaint"]/table/tbody/tr[%s]/td[4]/a'
+
+publish_content_xpath = '//*[@id="pl_service_common"]/div[4]/div[2]/div/div/div/div/p'
+publish_content_link_xpath = publish_content_xpath + '/a'
+publish_content_without_original_xpath = '//*[@id="pl_service_common"]/div[4]/div[2]/div/div/div/div/div/div'
+
+original_text_xpath = '//*[@id="app"]/div[1]/div[2]/div[2]/main/div[1]/div/div[2]/article/div[2]/div/div[2]/div'
