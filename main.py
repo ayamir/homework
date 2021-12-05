e2fee304587bb3f097f27ec49e29a33d52a3051f
ayamir <lgt986452565@gmail.com>
2021-12-05 11:38:27 +0800

    fix(crawl): fix situation no original link.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 import time
+from xpath import *
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,15 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
+url = "https://service.account.weibo.com/index?type=5&status=4&page=1"
 
 delay = 10
-
-dm_check_xpath = '//*[@id="dmCheck"]'
-
-publish_paragraph_xpath = '//*[@id="pl_service_common"]/div[4]/div[2]/div/div/div/div/p'
-publish_paragraph_link_xpath = publish_paragraph_xpath + '/a'
-
-original_text_xpath = '//*[@id="app"]/div[1]/div[2]/div[2]/main/div[1]/div/div[2]/article/div[2]/div/div[2]/div'
 
 
 def wait_for_loaded(driver, delay, xpath):
@@ -30,23 +25,30 @@ def wait_for_loaded(driver, delay, xpath):
 
 
 def get_info(driver):
-    win_path = '//*[@id="pl_service_common"]/div[2]/div[1]/div/div[3]'
-    if not check_exits_by_xpath(driver, win_path):
-        return None, None
+    win_xpath = '//*[@id="pl_service_common"]/div[2]/div[1]/div/div[3]'
+    publish_time, original_text = "", ""
+    if not check_exits_by_xpath(driver, win_xpath):
+        return publish_time, original_text
     else:
         publish_content = driver.find_element_by_xpath(
-            publish_paragraph_xpath).text
+            publish_content_xpath).text
         time_pattern = re.compile(r'\d+')
         date = ('-'.join(time_pattern.findall(publish_content)[0:3]))
         time = (':'.join(time_pattern.findall(publish_content)[3:6]))
         publish_time = date + " " + time
-        original_link = driver.find_element_by_xpath(
-            publish_paragraph_link_xpath).get_attribute('href')
-        driver.get(original_link)
-        original_element = wait_for_loaded(driver, delay, original_text_xpath)
-        if original_element is not None:
-            original_text = original_element.text
-            return original_text, publish_time
+        if check_exits_by_xpath(driver, publish_content_link_xpath):
+            original_link = driver.find_element_by_xpath(
+                publish_content_link_xpath).get_attribute('href')
+            driver.get(original_link)
+            original_element = wait_for_loaded(
+                driver, delay, original_text_xpath)
+            if original_element is not None:
+                original_text = original_element.text
+                driver.back()
+        else:
+            original_text = driver.find_element_by_xpath(
+                original_text_xpath).text
+        return original_text, publish_time
 
 
 def check_exits_by_xpath(driver, xpath):
@@ -69,8 +71,7 @@ wb.implicitly_wait(3)
 
 loginname = wb.find_element_by_id('loginname')
 password = wb.find_element_by_name('password')
-login_button = wb.find_element_by_xpath(
-    '//*[@id="pl_login_form"]/div/div[3]/div[6]/a')
+login_button = wb.find_element_by_xpath(login_button_xpath)
 
 
 loginname.send_keys("15910526673")
@@ -81,17 +82,18 @@ element = wait_for_loaded(wb, delay, dm_check_xpath)
 if element is not None:
     element.click()
 
-wb.find_element_by_xpath('//*[@id="send_dm_btn"]').click()
+wb.find_element_by_xpath(dm_send_button_xpath).click()
 time.sleep(delay)
-wb.get("https://service.account.weibo.com/index?type=5&status=4&page=1")
+wb.get(url)
 wb.implicitly_wait(3)
 
 for i in range(2, 22):
     wb.implicitly_wait(3)
-    info_element = wb.find_element_by_xpath(
-        '//*[@id="pl_service_showcomplaint"]/table/tbody/tr[%s]/td[2]/div/a' % i)
-    info_link = info_element.get_attribute('href')
+    info_link = wb.find_element_by_xpath(
+        info_link_xpath %
+        i).get_attribute('href')
     wb.get(info_link)
     info_content, info_time = get_info(wb)
+    print(info_content, info_time)
     wb.implicitly_wait(3)
     wb.back()
